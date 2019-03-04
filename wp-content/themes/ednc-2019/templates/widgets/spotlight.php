79ac1f077988957b124225c597864747424a34ba
Elizabeth Shealy <elizabeth.shealy@gmail.com>
2019-03-04 08:42:39 -0500

    updates

--- wp-content/themes/ednc-2019/templates/widgets/spotlight.php
+++ wp-content/themes/ednc-2019/templates/widgets/spotlight.php
@@ -15,9 +15,9 @@ $featured_image = Media\get_featured_image('medium');
         <?php $header = get_sub_field('header'); ?>
         <?php $image = get_sub_field('image'); ?>
         <h2 class="header-big">
-            <!-- <?php// if ($image){ ?>
-              <img class="section-icon" src="<?php// echo $image['url']; ?>" alt="<?php// echo $image['alt'] ?>" />
-            <?php } ?> -->
+            <?php// if ($image){ ?>
+              <!-- <img class="section-icon" src="<?php// echo $image['url']; ?>" alt="<?php// echo $image['alt'] ?>" /> -->
+            <?php// } ?>
             <img class="section-icon" src="<?php echo Assets\asset_path('images/spotlight.svg'); ?>">
             <?php echo $header ?>
             <?php echo get_cat_name($category); ?>
